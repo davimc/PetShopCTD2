0bc9107baec0678071fec22eb9b8d40c288226f2
davimc <davimatosc@hotmail.com>
2023-02-06 22:03:18 -0300

    Created

---
+++ backend/src/main/java/br/com/ctd/PetShopCTD2/PetShopCtd2Application.java
@@ -0,0 +1,13 @@
+package br.com.ctd.PetShopCTD2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopCtd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShopCtd2Application.class, args);
+	}
+
+}
